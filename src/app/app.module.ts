31dab7f495293fab41b4ded9ccc4ac4b21f06931
gtgitdev <g.turton@hotmail.com>
2019-02-23 07:44:12 -0500

    updated loading spinner component symbol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { NavBarComponent }  from './nav-bar.component';
 import { CatalogComponent } from "./catalog/catalog.component";
 import { RegisterComponent } from "./users/register.component";
 import { SignInComponent } from "./users/sign-in.component";
-import { LoadingComponent } from "./components/loading-spinner.component";
+import { LoadingSpinnerComponent } from "./components/loading-spinner.component";
 import { UserRepositoryService } from "./services/user-repository.service"
 import { CatalogRepositoryService } from './catalog/catalog-repository.service';
 
@@ -30,7 +30,7 @@ import { AccountMenuComponent } from "./account-menu.component";
     CatalogComponent,
     RegisterComponent,
     SignInComponent,
-    LoadingComponent,
+    LoadingSpinnerComponent,
     AccountMenuComponent
   ],
   providers: [ UserRepositoryService, CatalogRepositoryService ],
